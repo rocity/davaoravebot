6bad2d60f378d8348cccac0e91a064f2f4404e85
Kevin Paul Kalis <hello@kvnpl.xyz>
2016-08-09 11:53:12 +0800

    Add interval to do tasks

--- bot.js
+++ bot.js
@@ -18,57 +18,82 @@ var logs = 'logs.json';
 
 var hashtag_list = undefined;
 
-// get hashtag list
-hashtag_list = jsonfile.readFileSync(hashtags);
-messagelogs_list = jsonfile.readFileSync(logs);
+retweeter = function () {
+  // get hashtag list
+  hashtag_list = jsonfile.readFileSync(hashtags);
+  messagelogs_list = jsonfile.readFileSync(logs);
 
-eachAsync(hashtag_list.hashtags, (hashtag, index, done) => {
-  var q_tag = hashtag.tag,
-      q_since = '',
-      q_count = 10;
-  if (typeof hashtag.last_id !== 'undefined') {
-    // use date here
-    q_since = hashtag.last_id;
-  } else {
-    q_since = hashtag.last_date;
-  }
-
-  // execute tweet
-  var search_query = q_tag + ' since:' + q_since;
-  T.get('search/tweets', { q: search_query, count: q_count }, function(err, data, response) {
-
-    if (data.statuses.length) {
-
-      var lasttweet = _.last(data.statuses);
-
-      // LOGGING
-      var newfile = 'saves/' + q_tag + '_' + moment().format('MMMM_Do_YYYY_h_mm_ss_a') + '.json';
-
-      // create new log message
-      var msgs = messagelogs_list;
-      msgs.push('Write file executed for ' + newfile);
-
-      // create tweet data file
-      jsonfile.writeFile(newfile, data, function (err) {});
-      // END OF LOGGING
-
-      // execute POST commands here
-
-      // execute an RT for the last tweet
-      T.post('statuses/retweet/:id', { id: lasttweet.id_str }, function (err, data, response) {
-        msgs.push('Retweeted ' + lasttweet.id_str + ' - Timestamp: ' + moment().format());
-      })
-
-      // write message log at end of file
-      jsonfile.writeFileSync(logs, msgs);
-      done();
+  eachAsync(hashtag_list.hashtags, (hashtag, index, done) => {
+    var q_tag = hashtag.tag,
+        q_since = '',
+        q_count = 10;
+    if (typeof hashtag.last_id !== 'undefined') {
+      // use date here
+      q_since = hashtag.last_id;
     } else {
-      // there are no tweets yet.
-      var msgs = messagelogs_list;
-      msgs.push('No tweets yet. - Logged at ' + moment().format());
-      jsonfile.writeFileSync(logs, msgs);
+      q_since = hashtag.last_date;
     }
+
+    // execute tweet
+    var search_query = q_tag + ' since:' + q_since;
+    T.get('search/tweets', { q: search_query, count: q_count }, function(err, data, response) {
+
+      if (data.statuses.length) {
+
+        var lasttweet = _.last(data.statuses);
+
+        // LOGGING
+        var newfile = 'saves/' + q_tag + '_' + moment().format('MMMM_Do_YYYY_h_mm_ss_a') + '.json';
+
+        // create new log message
+        var msgs = messagelogs_list;
+        msgs.push('Write file executed for ' + newfile);
+
+        // create tweet data file
+        jsonfile.writeFile(newfile, data, function (err) {});
+        // END OF LOGGING
+
+        // execute POST commands here
+
+        // execute an RT for the last tweet
+        T.post('statuses/retweet/:id', { id: lasttweet.id_str }, function (err, data, response) {
+          msgs.push('Retweeted ' + lasttweet.id_str + ' - Timestamp: ' + moment().format());
+        })
+
+        // write message log at end of file
+        jsonfile.writeFileSync(logs, msgs);
+        done();
+      } else {
+        // there are no tweets yet.
+        var msgs = messagelogs_list;
+        msgs.push('No tweets yet. - Logged at ' + moment().format());
+        jsonfile.writeFileSync(logs, msgs);
+      }
+    })
+  }, error => {
+    console.log('Finished all processes.')
   })
-}, error => {
-  console.log('Finished all processes.')
-})
+}
+
+tweeter = function () {
+
+}
+
+// set intervals
+retweeterRun = function() {
+  async.waterfall([
+    retweeter
+  ],
+  function(err, botData) {
+
+  });
+};
+
+setInterval(function() {
+  try {
+    retweeterRun();
+  }
+  catch (e) {
+    console.log(e);
+  }
+}, 60000 * 10); // run every 3 minutes
